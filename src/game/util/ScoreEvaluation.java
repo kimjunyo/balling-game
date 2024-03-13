8ebe1134da087bdd4fe628d2de1566c430032dc6
kimjunyo <kimjunyo@github.com>
2024-03-14 02:24:47 +0900

    Fix: 0 점수를 얻은 라운드 점수 입력 오류 해결

--- src/game/util/ScoreEvaluation.java
+++ src/game/util/ScoreEvaluation.java
@@ -117,6 +117,7 @@ public class ScoreEvaluation {
 
     private static int frame2(String score) {
         String[] split = score.split("\\|");
+        if (score.equals("-")) return 0;
         if (split[0].equals("-")) return Integer.parseInt(split[1]);
         if (split[1].equals("-")) return Integer.parseInt(split[0]);
         if (split[1].equals("/")) return 10;
